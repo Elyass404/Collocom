5246c525f0d59bcbf39d1101a18bab2a1e073129
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-03-11 11:27:33 +0000

    made the logout functionality in the project

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,4 +56,14 @@ public function login(LoginRequest $request){
 
 }
 
+public function logout(Request $request)
+{
+    Auth::logout();
+
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return redirect()->route('login')->with('success', 'You have been logged out.');
+}
+
 }
