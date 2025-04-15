23b2e320f6961f11b0c3990c2eb9b47bebbcd523
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-04-15 15:28:03 +0100

    added the element into the category management in the side bar component

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -19,6 +19,14 @@
                 </a>
             </li>
             <li>
+                <a href="{{ route('categories.index') }}" class="flex items-center p-2 text-gray-700 hover:bg-gray-100 rounded">
+                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                    </svg>
+                    Manage Categories
+                </a>
+            </li>
+            <li>
                 <a href="{{ route('tags.index') }}" class="flex items-center p-2 text-gray-700 hover:bg-gray-100 rounded">
                     <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A2 2 0 013 12V7a4 4 0 014-4z"></path>
