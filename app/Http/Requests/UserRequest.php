4314c7a8112be08fcd7978503c5e76f414d0e0f6
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-04-18 12:46:21 +0100

    added the column of phone_number in the users table, and showed the phone number of the offer's owner, also modified the request of the user to verify the the inserted phone number

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -31,6 +31,15 @@ class UserRequest extends FormRequest
             'birthdate' => 'nullable|date|before:' . now()->subYears(5)->format('Y-m-d'), //this to make sure that the one who is registering is at least 5 years old
             'bio'       => 'nullable|string|max:1000',
             'situation_id' => 'nullable|string|exists:situations,id',
+            'phone_number' => "string|required|min:8|max:15|unique:users,phone_number|regex:/^\+?[0-9\s\-]+$/",
+
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'phone_number.regex'=>"Phone number format is invalid. Use only numbers, spaces, +, and dashes."
         ];
     }
 }
