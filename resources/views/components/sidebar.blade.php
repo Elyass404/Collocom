c28f6aac19929d5f0ed7a4ecdf26439736d044a8
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-05-09 11:34:41 +0100

    added the (manage roles) tab in the side  bar for iin the dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -19,6 +19,14 @@
                 </a>
             </li>
             <li>
+                <a href="{{ route('roles.index') }}" class="flex items-center p-2 text-gray-700 hover:bg-gray-100 rounded">
+                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
+                    </svg>
+                    Manage Roles
+                </a>
+            </li>
+            <li>
                 <a href="{{ route('categories.index') }}" class="flex items-center p-2 text-gray-700 hover:bg-gray-100 rounded">
                     <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
